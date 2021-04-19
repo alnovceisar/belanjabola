d8e224f75d094df8269cb66721530d013b215bcf
Alnov Ceisar <alnovceisar@gmail.com>
2021-04-19 23:23:20 +0700

    initial commit

---
+++ belanjabola.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "650px";
+    }
+
+    function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+    }
